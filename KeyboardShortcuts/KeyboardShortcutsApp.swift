a7655e05f040799f98852574928808eb7987fe5a
Christina Alperi <christinaalperi@gmail.com>
2022-09-21 18:48:37 -0700

    Initial Commit

---
+++ KeyboardShortcuts/KeyboardShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardShortcutsApp.swift
+//  KeyboardShortcuts
+//
+//  Created by Christina Alperi on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
